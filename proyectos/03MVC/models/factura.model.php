44ceda704e411a8a4fc59688cdd88c3f78e11db4
adrianam3 <adrian_am3@hotmail.com>
2024-08-11 12:40:23 -0500

    Generar CRUDs de factura y detalle factura, model y controller

---
+++ proyectos/03MVC/models/factura.model.php
@@ -0,0 +1,79 @@
+<?php
+//TODO: Clase de Factura
+require_once('../config/config.php');
+class Factura
+{
+    //TODO: Implementar los metodos de la clase
+
+    public function todos() //select * from factura
+    {
+        $con = new ClaseConectar();
+        $con = $con->ProcedimientoParaConectar();
+        $cadena = "SELECT * FROM `factura`";
+        $datos = mysqli_query($con, $cadena);
+        $con->close();
+        return $datos;
+    }
+
+    public function uno($idFactura) //select * from factura where id = $id
+    {
+        $con = new ClaseConectar();
+        $con = $con->ProcedimientoParaConectar();
+        $cadena = "SELECT * FROM `factura` WHERE `idFactura`=$idFactura";
+        $datos = mysqli_query($con, $cadena);
+        $con->close();
+        return $datos;
+    }
+
+    public function insertar($Fecha, $Sub_total, $Sub_total_iva, $Valor_IVA, $Clientes_idClientes) //INSERT INTO `factura`(`idFactura`, `Fecha`, `Sub_total`, `Sub_total_iva`, `Valor_IVA`, `Clientes_idClientes`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]')
+    {
+        try {
+            $con = new ClaseConectar();
+            $con = $con->ProcedimientoParaConectar();
+            $cadena = "INSERT INTO `factura` ( `Fecha`, `Sub_total`, `Sub_total_iva`, `Valor_IVA`, `Clientes_idClientes`) VALUES ('$Fecha','$Sub_total','$Sub_total_iva','$Valor_IVA','$Clientes_idClientes')";
+            if (mysqli_query($con, $cadena)) {
+                return $con->insert_id;
+            } else {
+                return $con->error;
+            }
+        } catch (Exception $th) {
+            return $th->getMessage();
+        } finally {
+            $con->close();
+        }
+    }
+    public function actualizar($idFactura, $Fecha, $Sub_total, $Sub_total_iva, $Valor_IVA, $Clientes_idClientes) //UPDATE `factura` SET `idFactura`='[value-1]',`Fecha`='[value-2]',`Sub_total`='[value-3]',`Sub_total_iva`='[value-4]',`Valor_IVA`='[value-5]',`Clientes_idClientes`='[value-6]' WHERE 1
+    {
+        try {
+            $con = new ClaseConectar();
+            $con = $con->ProcedimientoParaConectar();
+            $cadena = "UPDATE `factura` SET `idFactura`='$idFactura',`Fecha`='$Fecha',`Sub_total_iva`='$Sub_total_iva',`Valor_IVA`='$Valor_IVA',`Clientes_idClientes`='$Clientes_idClientes' WHERE `idFactura` = $idFactura";
+            if (mysqli_query($con, $cadena)) {
+                return $idFactura;
+            } else {
+                return $con->error;
+            }
+        } catch (Exception $th) {
+            return $th->getMessage();
+        } finally {
+            $con->close();
+        }
+    }
+    public function eliminar($idFactura) //delete from factura where idFactura = $idFactura
+    {
+        try {
+            $con = new ClaseConectar();
+            $con = $con->ProcedimientoParaConectar();
+            $cadena = "DELETE FROM `factura` WHERE `idFactura`= $idFactura";
+            if (mysqli_query($con, $cadena)) {
+                return 1;
+            } else {
+                return $con->error;
+            }
+        } catch (Exception $th) {
+            return $th->getMessage();
+        } finally {
+            $con->close();
+        }
+    }
+}
